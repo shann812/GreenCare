c161c126b3442eb210b614ed7ae355fb5e7caa91
ккрк огкои <shandra.ilya@gmail.com>
2026-01-30 14:59:31 +0200

    chore: initial EF

---
+++ backend/GreenCareApi/GreenCareApi/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+public class ApplicationDbContext : DbContext
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options)
+    {
+	}
+
+    //models will go here
+}
